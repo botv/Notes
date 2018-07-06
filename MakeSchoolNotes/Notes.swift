4ea39faec1780840fe568280edb26e5494038055
botvinick <benbotvinick@gmail.com>
2018-07-05 19:53:51 -0700

    Add everything

---
+++ MakeSchoolNotes/Notes.swift
@@ -0,0 +1,18 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Ben Botvinick on 7/5/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+
+    // MARK: - Properties
+
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
